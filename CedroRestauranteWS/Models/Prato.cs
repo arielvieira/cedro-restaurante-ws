eff2ab900ecd99c1a4a1971f12f5e09674b7ef70
Ariel Vieira <arielvieirasv@gmail.com>
2018-04-30 03:47:19 -0300

    Add models classes

---
+++ CedroRestauranteWS/Models/Prato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CedroRestauranteWS.Models
+{
+    public class Prato
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double Preco { get; set; }
+
+        public Restaurante Restaurante { get; set; }
+    }
+}
---
+++ CedroRestauranteWS/Models/Restaurante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CedroRestauranteWS.Models
+{
+    public class Restaurante
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        public IList<Prato> Pratos { get; set; }
+    }
+}
