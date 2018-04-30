58140659aeea2c1b494d4d1e70cb0f5253dd614c
Ariel Vieira <arielvieirasv@gmail.com>
2018-04-30 04:14:08 -0300

    Add InitialMigration

---
+++ CedroRestauranteWS/Migrations/20180430071203_InitialMigration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CedroRestauranteWS.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurantes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pratos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false),
+                    Preco = table.Column<double>(nullable: false),
+                    RestauranteId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pratos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pratos_Restaurantes_RestauranteId",
+                        column: x => x.RestauranteId,
+                        principalTable: "Restaurantes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pratos_RestauranteId",
+                table: "Pratos",
+                column: "RestauranteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pratos");
+
+            migrationBuilder.DropTable(
+                name: "Restaurantes");
+        }
+    }
+}
